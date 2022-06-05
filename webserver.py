4a624e827b8820d86424d1ef8fb3c0a16b65a26e
Elon Musk <91766805+Elon-Musk-YAY@users.noreply.github.com>
2022-06-05 12:18:11 +0000

    Updating Flask

--- webserver.py
+++ webserver.py
@@ -15,67 +15,66 @@ def home():
     return "Discord bot is OK and running\nWebserver is OK and ready to be pinged!"
 
 
-@app.route('/files/1')
+# @app.route('/files/1')
 
-def jobs():
-    with open('databases/jobs.json','r') as f:
-        text = f.read()
-    return text
+# def jobs():
+#     with open('databases/jobs.json','r') as f:
+#         text = f.read()
+#     return text
 
-@app.route('/files/2')
+# @app.route('/files/2')
 
-def levels():
-    with open('databases/levels.json','r') as f:
-        text = f.read()
-    return text
-
-
-@app.route('/files/3')
-
-def lootboxes():
-    with open('databases/lootboxes.json','r') as f:
-        text = f.read()
-    return text
-
-@app.route('/files/4')
-
-def bank():
-    with open('databases/mainbank.json','r') as f:
-        text = f.read()
-    return text
+# def levels():
+#     with open('databases/levels.json','r') as f:
+#         text = f.read()
+#     return text
 
 
-@app.route('/files/5')
+# @app.route('/files/3')
 
-def prefixes():
-    with open('databases/prefixes.json','r') as f:
-        text = f.read()
-    return text
+# def lootboxes():
+#     with open('databases/lootboxes.json','r') as f:
+#         text = f.read()
+#     return text
 
-@app.route('/files/6')
+# @app.route('/files/4')
 
-def role():
-    with open('databases/reactrole.json','r') as f:
-        text = f.read()
-    return text
+# def bank():
+#     with open('databases/mainbank.json','r') as f:
+#         text = f.read()
+#     return text
+
+
+# @app.route('/files/5')
+
+# def prefixes():
+#     with open('databases/prefixes.json','r') as f:
+#         text = f.read()
+#     return text
+
+# @app.route('/files/6')
+
+# def role():
+#     with open('databases/reactrole.json','r') as f:
+#         text = f.read()
+#     return text
 
 
 
-@app.route('/files/7')
+# @app.route('/files/7')
 
-def conf():
-    with open('databases/server_configs.json','r') as f:
-        text = f.read()
-    return text
+# def conf():
+#     with open('databases/server_configs.json','r') as f:
+#         text = f.read()
+#     return text
 
 
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=8080)
 
 
-def keep_alive():  
-
+def keep_alive():
     t = Thread(target=run)
 
     t.start()
