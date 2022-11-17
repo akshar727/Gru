d32c3d168dcf9404be98f94e03a7cde340eb1d46
Elon Musk <91766805+Elon-Musk-YAY@users.noreply.github.com>
2022-11-17 12:54:12 +0000

    checks

--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -8,10 +8,3 @@ load_dotenv()
 def getenv(key):
     return os.getenv(key)
 
-def save_json(filename, data):
-    with open(filename, "w") as f:
-        json.dump(data,f)
-
-def open_json(filename):
-    with open(filename, "r") as f:
-        return json.loads(f.read())
